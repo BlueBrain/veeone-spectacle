81df67290a43afbcd6499ec42b501adac3b8e3a6
Marek Naskret <marek.naskret@epfl.ch>
2021-04-14 20:04:03 +0200

    Implement empty app scaffold

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Spectacle`,
+    description: ``,
+    author: `Blue Brain Project`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -16,19 +16,19 @@ module.exports = {
     },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
-        start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
-      },
-    },
+    // {
+    //   resolve: `gatsby-plugin-manifest`,
+    //   options: {
+    //     name: `gatsby-starter-default`,
+    //     short_name: `starter`,
+    //     start_url: `/`,
+    //     background_color: `#663399`,
+    //     theme_color: `#663399`,
+    //     display: `minimal-ui`,
+    //   },
+    // },
     `gatsby-plugin-gatsby-cloud`,
+    `gatsby-plugin-sass`,
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
---
+++ src/components/Spectacle.tsx
@@ -0,0 +1,14 @@
+import * as React from "react"
+
+import * as styles from "./Spectacle.module.scss"
+
+const Spectacle = () => {
+  return (
+    <div className={styles.Spectacle}>
+      {/*<Desk></Desk>*/}
+      Spectacle App
+    </div>
+  )
+}
+
+export default Spectacle
