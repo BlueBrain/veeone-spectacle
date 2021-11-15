663497a3aefd3616c798d8d7801512932712d993
Marek Naskret <marek.naskret@epfl.ch>
2021-11-15 13:37:46 +0100

    Allow overriding config (add "conf" as encoded JSON object to query params)

--- src/config/app-config.ts
+++ src/config/app-config.ts
@@ -1,3 +1,6 @@
+import queryParamOverrides from "./query-param-overrides"
+import { ApplicationConfig } from "./types"
+
 declare const ENV_VARIABLES: any
 
 console.debug("ENV_VARIABLES", ENV_VARIABLES)
@@ -6,9 +9,15 @@ const defaults = {
   VEEDRIVE_WS_PATH: "ws://localhost:4444/ws",
 }
 
-const AppConfig = {
+const AppConfig: ApplicationConfig = {
   VEEDRIVE_WS_PATH:
     ENV_VARIABLES.SPECTACLE_VEEDRIVE_WS_PATH ?? defaults.VEEDRIVE_WS_PATH,
+  FILE_BROWSER_WIDTH: 700,
+  FILE_BROWSER_HEIGHT: 400,
 }
 
-export default AppConfig
+const AppConfigWithOverrides = queryParamOverrides.wrap(AppConfig)
+
+console.debug("AppConfigWithOverrides", AppConfigWithOverrides)
+
+export default AppConfigWithOverrides
---
+++ src/config/types.ts
@@ -0,0 +1,5 @@
+export interface ApplicationConfig {
+  VEEDRIVE_WS_PATH: string
+  FILE_BROWSER_WIDTH: number
+  FILE_BROWSER_HEIGHT: number
+}
