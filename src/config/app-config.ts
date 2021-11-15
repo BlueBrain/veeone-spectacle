294779bcfcbbdcfef6caa40ccecdaa11939dd99d
Marek Naskret <marek.naskret@epfl.ch>
2021-11-15 13:41:08 +0100

    Set default values in config for file browser and viewport

--- src/config/app-config.ts
+++ src/config/app-config.ts
@@ -6,14 +6,19 @@ declare const ENV_VARIABLES: any
 console.debug("ENV_VARIABLES", ENV_VARIABLES)
 
 const defaults = {
+  VIEWPORT_WIDTH: window.visualViewport.width,
+  VIEWPORT_HEIGHT: window.visualViewport.height,
   VEEDRIVE_WS_PATH: "ws://localhost:4444/ws",
+  FILE_BROWSER_WIDTH: window.visualViewport.width / 3,
+  FILE_BROWSER_HEIGHT: window.visualViewport.height / 3,
 }
 
 const AppConfig: ApplicationConfig = {
+  ...defaults,
   VEEDRIVE_WS_PATH:
     ENV_VARIABLES.SPECTACLE_VEEDRIVE_WS_PATH ?? defaults.VEEDRIVE_WS_PATH,
-  FILE_BROWSER_WIDTH: 700,
-  FILE_BROWSER_HEIGHT: 400,
+  VIEWPORT_WIDTH: window.visualViewport.width,
+  VIEWPORT_HEIGHT: window.visualViewport.height,
 }
 
 const AppConfigWithOverrides = queryParamOverrides.wrap(AppConfig)
--- src/config/types.ts
+++ src/config/types.ts
@@ -2,4 +2,6 @@ export interface ApplicationConfig {
   VEEDRIVE_WS_PATH: string
   FILE_BROWSER_WIDTH: number
   FILE_BROWSER_HEIGHT: number
+  VIEWPORT_WIDTH: number
+  VIEWPORT_HEIGHT: number
 }
