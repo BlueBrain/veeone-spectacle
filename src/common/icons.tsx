3e8ba9c68c82873cee0f8e9a7c6dd123bcfe62dc
Marek Naskret <marek.naskret@epfl.ch>
2021-11-24 22:06:14 +0100

    Remove icons.tsx (redundant module)

--- src/common/icons.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react"
-
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-import { faCoffee, faFolderOpen } from "@fortawesome/free-solid-svg-icons"
-
-export const CoffeeIcon = () => <FontAwesomeIcon icon={faCoffee} />
-
-export const FolderOpenIcon = () => <FontAwesomeIcon icon={faFolderOpen} />
