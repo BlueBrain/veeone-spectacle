8dd964826bd360ad3f35c0a438a307a747952312
Marek Naskret <marek.naskret@epfl.ch>
2022-03-08 10:03:55 +0100

    Add BBP logo

--- src/core/desk/DeskBranding.tsx
+++ src/core/desk/DeskBranding.tsx
@@ -16,8 +16,34 @@ const useStyles = makeStyles({
 export const DeskBranding: React.FC<DeskBrandingProps> = () => {
   const classes = useStyles()
   return (
-    <Box sx={{ position: "absolute", top: 0, left: 0 }}>
-      <img alt={""} src={EPFLLogo} className={classes.logo} />
+    <Box
+      sx={{
+        position: "absolute",
+        top: 0,
+        left: 0,
+        display: "flex",
+        alignItems: "center",
+      }}
+    >
+      <Box>
+        <Box
+          component={"img"}
+          src={EPFLLogo}
+          sx={{
+            height: "1.2vh",
+            margin: "2vh",
+          }}
+        />
+      </Box>
+      <Box
+        sx={{
+          fontSize: "1.2rem",
+          paddingBottom: "0.3rem",
+          color: theme => theme.branding.main,
+        }}
+      >
+        Blue Brain Project
+      </Box>
     </Box>
   )
 }
