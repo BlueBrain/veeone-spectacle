f19d95199c11902501d75701d217e995e94af650
Marek Naskret <marek.naskret@epfl.ch>
2022-08-17 17:00:40 +0200

    Keep dialogs windows within the desk safety area

--- src/dialogs/SpectacleDialog.tsx
+++ src/dialogs/SpectacleDialog.tsx
@@ -1,12 +1,39 @@
-import { Dialog } from "@mui/material"
-import React, { useEffect, useRef } from "react"
+import { Dialog, Paper, PaperProps } from "@mui/material"
+import React, { useCallback, useEffect, useRef } from "react"
 import { BaseDialog, DialogOptions } from "./DialogsContext"
+import { useConfig } from "../config/AppConfigContext"
+import { ApplicationConfig } from "../config/types"
 
 interface SpectacleDialogProps {
   component: React.FC<BaseDialog> | React.NamedExoticComponent<BaseDialog>
   options: DialogOptions
   resolve: (value: any) => void
   reject: (reason: any) => void
+}
+
+const fixBoxPosition = (ref: HTMLDivElement, config: ApplicationConfig) => {
+  // TODO fix this
+  const parentRect = ref.offsetParent.getBoundingClientRect()
+  console.debug("PARENT DESK", parentRect)
+  const leftEdge = ref.offsetLeft - ref.offsetWidth / 2
+  const rightEdge = parentRect.width - ref.offsetLeft + ref.offsetWidth / 2
+  const topEdge = ref.offsetTop - ref.offsetHeight / 2
+  const bottomEdge = ref.offsetHeight / 2 - ref.offsetTop
+  console.debug("BOX CHECK", { leftEdge, topEdge, rightEdge, bottomEdge })
+}
+
+const PaperComponent: React.FC<PaperProps> = props => {
+  const paperRef = useRef()
+  const config = useConfig()
+  useEffect(() => {
+    const currentPaperRef = paperRef.current as HTMLDivElement
+    if (currentPaperRef) {
+      // Correct paper component (dialog window) position if too close to the edge
+      fixBoxPosition(currentPaperRef, config)
+    }
+  })
+
+  return <Paper {...props} ref={paperRef} />
 }
 
 const SpectacleDialog: React.FC<SpectacleDialogProps> = ({
@@ -18,19 +45,14 @@ const SpectacleDialog: React.FC<SpectacleDialogProps> = ({
 }) => {
   const { fullWidth = true, maxWidth = "sm", position } = options
   const DialogComponent = component
-  const dialogRef = useRef()
-
-  useEffect(() => {
-    console.debug("dialogRef", dialogRef.current)
-  }, [])
 
   return (
     <Dialog
-      ref={dialogRef}
       open={true}
       onClose={reject}
       maxWidth={maxWidth}
       fullWidth={fullWidth}
+      PaperComponent={PaperComponent}
       PaperProps={{
         sx: {
           position: "absolute",
